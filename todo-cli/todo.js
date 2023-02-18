f46d14b1677f13136a87c19979fde0806ac8ad50
devang-yagnik <devangyagnik865@gmail.com>
2023-02-19 02:24:56 +0530

    test commit

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -64,4 +64,17 @@ const todoList = () => {
   };
 };
 
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
+
 module.exports = todoList;
