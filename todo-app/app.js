4722cf64570a715780966b46f10e9c31bec33cd9
devang-yagnik <devangyagnik865@gmail.com>
2023-03-26 00:41:01 +0530

    deploy

--- todo-app/app.js
+++ todo-app/app.js
@@ -3,13 +3,40 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+// const path = require("path");
 app.use(bodyParser.json());
+
+app.set("view engine", "ejs")
+
+// app.use(express.static(path.join(__dirname,'public')))
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodoList()
+  if(request.accepts("html")){
+    response.render('index', {
+      allTodos
+    })
+  }
+  else{
+    response.json(allTodos)
+  }
+})
 
 app.get("/todos", async (request, response) => {
   // response.send("hello world")
   console.log("Todo list");
   const todoList = await Todo.getTodoList()
   return response.json(todoList)
+});
+
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 app.post("/todos", async (request, response) => {
